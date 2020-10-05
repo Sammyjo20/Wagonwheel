418e569227a219b3647cb31bcfbfd519d9d9b24c
Sammyjo20 <sam.carre2000@gmail.com>
2020-10-05 16:44:04 +0100

    Removed divz

--- src/Views/components/view-online.blade.php
+++ src/Views/components/view-online.blade.php
@@ -1,5 +1,3 @@
-<div>
-    <p style="width: 100%; margin: 0 auto;">
-        <a href="{{ $url }}" style="display: block; text-align: center;">View online</a>
-    </p>
-</div>
+<p style="width: 100%; margin: 0 auto;">
+    <a href="{{ $url }}" style="display: block; text-align: center;">View online</a>
+</p>

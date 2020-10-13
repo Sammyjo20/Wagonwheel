35ea93a6a3b9b8d94b7e3da16fc09ded1a573fc1
John Braun <john@johnbraun.nl>
2020-10-13 20:40:24 +0200

    Import URL facade

--- src/Models/OnlineMailable.php
+++ src/Models/OnlineMailable.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Database\Factories\OnlineMailableFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
 
 class OnlineMailable extends Model
 {

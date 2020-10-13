7c6a5cb8509ba4d7c1bf876c48dd0dbd4824cadd
John Braun <john@johnbraun.nl>
2020-10-13 06:43:11 +0200

    Add (class based) model factory for OnlineMailable

--- src/Models/OnlineMailable.php
+++ src/Models/OnlineMailable.php
@@ -2,11 +2,20 @@
 
 namespace Sammyjo20\Wagonwheel\Models;
 
+use Database\Factories\OnlineMailableFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class OnlineMailable extends Model
 {
+    use HasFactory;
+
     protected $casts = [
         'expires_at' => 'timestamp',
     ];
+
+    protected static function newFactory()
+    {
+        return new OnlineMailableFactory;
+}
 }
